f01da5393bc43dd3ec337cf9a810cfe444678809
PRMcGill <mcgill.patrick2192@gmail.com>
2019-03-05 10:51:43 -0500

    added the add function in courses/views.py and changed the names in the urlpatterns in the course/urls.py file

--- course/urls.py
+++ course/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='course-home'),
-    path('add/', views.add, name='add-course')
+    path('', views.home, name='courseHome'),
+    path('add/', views.add, name='addCourse')
 ]
--- course/views.py
+++ course/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from . import models
 
 def home(request):
     return HttpResponse('<h1>Course Home</h1>')
 
 def add(request):
-    return HttpResponse('<h1>Course Add</h1>')
+
+    if request.method == "POST":
+        added_course = models.Item(course_name=request.POST['Course Name'],city=request.POST['City'], holes=request.POST['Holes'], tee_type=request.POST['Tee Type'])
+        added_course.save()
+        return redirect('courseHome')
+    
+    return render(request, 'courseHome/add')
