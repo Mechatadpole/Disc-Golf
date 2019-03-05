116e759087d85bd3dcca7d9d3158f85cea1a02d0
PRMcGill <mcgill.patrick2192@gmail.com>
2019-03-05 09:47:01 -0500

    made the course models class

--- course/models.py
+++ course/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    course_name = models.CharField(max_length=40)
+    city = models.CharField(max_length=40)
+    holes = models.IntegerField(min_val= 6, max_val= 36)
+    tee_type = models.CharField(max_length=8)
