10efba97181727da7864438b577120913f282fae
Owen Edwards <omedwards@outlook.com>
2019-03-05 11:19:50 -0500

    fixed minor changes in merging

--- disc_golf/settings.py
+++ disc_golf/settings.py
@@ -30,8 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [ 
-    'course.app.CourseConfig',  
+INSTALLED_APPS = [   
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'register',
+    'course',
 ]
 
 MIDDLEWARE = [
