4b5c350c02ace4e2a343ddc808095741ec63c185
PRMcGill <mcgill.patrick2192@gmail.com>
2019-03-05 10:00:10 -0500

    added validators to the holes part of the class

--- course/models.py
+++ course/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class Course(models.Model):
     course_name = models.CharField(max_length=40)
     city = models.CharField(max_length=40)
-    holes = models.IntegerField(min_val= 6, max_val= 36)
+    holes = models.IntegerField(validators=[MinValueValidator(6), MaxValueValidator(36)])
     tee_type = models.CharField(max_length=8)
