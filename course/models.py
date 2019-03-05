d246234a58191cac0998984a7cb959dfcb6d929a
Owen Edwards <omedwards@outlook.com>
2019-03-05 10:28:59 -0500

    Added in add path to course

--- course/models.py
+++ course/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
+
+
 class Course(models.Model):
     course_name = models.CharField(max_length=40)
     city = models.CharField(max_length=40)
