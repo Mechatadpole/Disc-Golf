154dfba7a88ed019ac336350a4af20aca3e1a599
Owen Edwards <omedwards@outlook.com>
2019-03-07 12:00:45 -0500

    added in comments on almost all files with content showing/displaying

--- login/apps.py
+++ login/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-
+# App name for the login section of the site, Login.
 class LoginConfig(AppConfig):
     name = 'login'
--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# All urls used to navigate the different paths in Pages, this being only the Home Page.
+
 urlpatterns = [
     path('', views.home, name='pages-home'),
 ]
