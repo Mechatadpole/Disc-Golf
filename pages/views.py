9c8358676ece4704721db43873e5961b8dbd11fe
Owen Edwards <omedwards@outlook.com>
2019-03-11 16:18:04 -0400

    About the creators html pages successfully pathed

--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import HttpResponse
 
 def home(request):
     return render(request, 'pages/home.html')
+
+def members(request):
+    return render(request, 'pages/group.html')
+
+def work(request):
+    return render(request, 'pages/process.html')
+
+def site(request):
+    return render(request, 'pages/goals.html')
