4e803aa07d672a759bdd9efbaee0a1149523c442
PRMcGill <mcgill.patrick2192@gmail.com>
2019-03-13 09:39:01 -0400

    i like to do the cha cha

--- course/urls.py
+++ course/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-# Alls urls used for navigating through the different Course pages.
+# Alls urls used for navigating through the different Course pages....
 
 urlpatterns = [
     path('', views.home, name='courseHome'),
