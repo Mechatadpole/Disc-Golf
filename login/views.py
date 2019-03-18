e0aa5e60711907dab04b615b1ac206c74ee125bd
Owen Edwards <omedwards@outlook.com>
2019-03-18 10:08:42 -0400

    I deleted files but typed git add lol

--- login/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# When the home function is requested, redirects users to the login page where they can login to the site.
-def home(request):
-    return render(request, 'login/login.html')
